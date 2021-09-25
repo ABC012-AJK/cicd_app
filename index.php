cc3b8ea7e6115165f7e144abea4db02e00042f81
Sanju2 <lsanjeewa947@gmail.com>
2021-09-25 12:56:21 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home Page</title>
+</head>
+
+<body>
+
+    <h1>Hello PHP</h1>
+
+    <?php echo "PHP Elastic Beanstalk && CodePipeline"; ?>
+
+</body>
+
+</html>
